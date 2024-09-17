0381649316fc8a837e52ba57e033b6a164e02b35
SartoRiccardo <rikki.sarto@gmail.com>
2024-09-18 01:31:21 +0200

    fix: Fixed formula

--- bot/utils/formulas.py
+++ bot/utils/formulas.py
@@ -9,7 +9,7 @@ def points(idx: int, config: dict) -> float:
     top = config['points_top_map']
     slp = config['formula_slope']
     amt = config['map_count']
-    result = btm + (top/btm) ** ((1 + ((1-idx)/(amt-1))) ** slp)
+    result = btm * pow(top/btm, pow(1 + ((1-idx)/(amt-1)), slp))
     round_to = config['decimal_digits']
     return round(result, round_to) if round_to else math.floor(result)
 
