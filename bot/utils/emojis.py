837d1d6049e71e1baba2931bd8d45243de2bfee8
Riccardo Sartori <riccardo.sartori@blueskyagent.ai>
2026-05-10 19:04:17 +0200

    feat: Production emojis

--- bot/utils/emojis.py
+++ bot/utils/emojis.py
@@ -7,12 +7,12 @@ class BaseEmjClass:
 
 
 class EmjIcons(BaseEmjClass):
-    casual = "🙂"
-    medium = "😌"
-    hard = "😏"
-    true = "😈"
-    extreme = "🔥"
-    packs = "📦"
+    casual = "<:i_casual:1284134327256616960>"
+    medium = "<:i_medium:1284134377155989506>"
+    hard = "<:i_hard:1284134361310036018>"
+    true = "<:i_true:1284134406432493569>"
+    extreme = "<:i_extreme:1293317962953920614>"
+    packs = "<:i_packs:1284134393585336331>"
 
     botb_beginner = "<:icon_botb_1:1361394050602762310>"
     botb_intermediate = "<:icon_botb_2:1361394005832630673>"
@@ -30,8 +30,8 @@ class EmjIcons(BaseEmjClass):
     np_btdb2 = "<:icon_np_8:1361394456292753449>"
     np_missing = "❓"
 
-    curver = "⏰"
-    allver = "🕰️"
+    curver = "<:i_curver:1284134344860106775>"
+    allver = "<:i_allver:1284134312664633374>"
     experts = hard
     botb_icon = "<:icon_botb:1361394250717073619>"
     np = np_btd123
@@ -59,36 +59,35 @@ class EmjIcons(BaseEmjClass):
 
 
 class EmjMedals(BaseEmjClass):
-    win = "🔴"
-    bb = "⚫"
-    no_opt_hero = "🎖️"
-    lcc = "🪙"
+    win = "<:m_win:1284093064247246921>"
+    bb = "<:m_bb:1284092992294092810>"
+    no_opt_hero = "<:m_noopthero:1284093053321089116>"
+    lcc = "<:m_lcc:1284093037391253526>"
 
 
-# In-game hero select icons
 class EmjHeros(BaseEmjClass):
-    quincy = "🦸‍♂️"
-    gwen = "🦸‍♂️"
-    obyn = "🦸‍♂️"
-    striker = "🦸‍♂️"
-    churchill = "🦸‍♂️"
-    ben = "🦸‍♂️"
-    ezili = "🦸‍♂️"
-    silas = "🦸‍♂️"
-    pat = "🦸‍♂️"
-    adora = "🦸‍♂️"
-    brickell = "🦸‍♂️"
-    etienne = "🦸‍♂️"
-    sauda = "🦸‍♂️"
-    psi = "🦸‍♂️"
-    geraldo = "🦸‍♂️"
-    corvus = "🦸‍♂️"
-    rosalia = "🦸‍♂️"
+    quincy = "<:h_quincy:1284204430199230534>"
+    gwen = "<:h_gwen:1284204379615924244>"
+    obyn = "<:h_obyn:1284204391804571759>"
+    striker = "<:h_striker:1284204469709705247>"
+    churchill = "<:h_churchill:1284204314151096343>"
+    ben = "<:h_ben:1284204290356940914>"
+    ezili = "<:h_ezili:1284204353221169233>"
+    pat = "<:h_pat:1284204408309284945>"
+    adora = "<:h_adora:1284204278562553957>"
+    brickell = "<:h_brickell:1284204302499577879>"
+    etienne = "<:h_etienne:1284204335336525856>"
+    sauda = "<:h_sauda:1284204458619834448>"
+    silas = "<:h_silas:1421227845706977400>"
+    psi = "<:h_psi:1284204418924810314>"
+    geraldo = "<:h_geraldo:1284204366429028423>"
+    corvus = "<:h_corvus:1284204325455007940>"
+    rosalia = "<:h_rosalia:1284204442270433365>"
 
 
 class EmjMisc(BaseEmjClass):
-    cash = "<a:cash:1148694623192105010>"  # BTD6 cash icon
-    blank = "<:_:1121392589204094976>"  # Completely transparent emoji
+    cash = "<a:cash:1148694623192105010>"
+    blank = "<:_:1121392589204094976>"
 
 
 class EmjPlacements(BaseEmjClass):
